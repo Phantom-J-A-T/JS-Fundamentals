2aa64861eedeb8f3641d81e5ed7bc6669993e297
Phantom <ramonadedotun550@gmail.com>
2025-05-21 14:17:24 +0100

    Task 2

---
+++ 2-arguments.js
@@ -0,0 +1,6 @@
+const args = process.argv.slice(2);
+const message = args.length === 0 ? "No argument" :
+                args.length === 1 ? "Argument found" :
+                                    "Arguments found";
+
+console.log(message);
